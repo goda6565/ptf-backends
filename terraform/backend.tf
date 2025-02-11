76f7f3d8d4c3fba65763f352bfb850eaf8d62b87
harutin <godaharuto@gmail.com>
2025-02-11 23:35:44 +0900

    fix s3 region

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "ptf-terraform-tfstate"
+    bucket         = "ptf-terraform-tfstate-backends"
     key            = "terraform.tfstate"
     region         = "ap-northeast-1"
     dynamodb_table = "ptf-terraform-tfstate-locking"
