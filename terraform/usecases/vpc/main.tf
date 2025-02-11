4034126623be9afd00d71dd5f0299f6fe249844e
harutin <godaharuto@gmail.com>
2025-02-12 00:10:38 +0900

    feat: add vpc

---
+++ terraform/usecases/vpc/main.tf
@@ -0,0 +1,26 @@
+data "aws_availability_zones" "current" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.18.1"
+
+  name = "${var.stage}-ptf-backend-vpc"
+  cidr = var.vpc_cidr
+
+  azs = slice(data.aws_availability_zones.current.names, 0, 2)
+  public_subnets = [
+    cidrsubnet(var.vpc_cidr, 4, 0),
+    cidrsubnet(var.vpc_cidr, 4, 1)
+  ]
+  private_subnets = [
+    cidrsubnet(var.vpc_cidr, 4, 2),
+    cidrsubnet(var.vpc_cidr, 4, 3)
+  ]
+
+  enable_nat_gateway     = var.enable_nat_gateway
+  single_nat_gateway     = var.enable_nat_gateway ? (var.one_nat_gateway_per_az ? false : true) : false
+  one_nat_gateway_per_az = var.enable_nat_gateway ? var.one_nat_gateway_per_az : false
+
+  enable_dns_hostnames = true # default value
+  enable_dns_support   = true # default value
+}
---
+++ terraform/usecases/vpc/variables.tf
@@ -0,0 +1,20 @@
+variable "stage" {
+  type        = string
+  description = "The stage of the environment"
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "The CIDR block for the VPC"
+}
+
+variable "enable_nat_gateway" {
+  type        = bool
+  description = "Enable NAT Gateway for private subnets"
+}
+
+variable "one_nat_gateway_per_az" {
+  type        = bool
+  description = "Enable one NAT Gateway per Availability Zone"
+  default     = false
+}
